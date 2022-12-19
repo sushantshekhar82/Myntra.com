e110f6359aaf7ebfe0fef6763a4377c850fbdfc3
sushant shekhar <sushant.shekhar151997@gmail.com>
2022-12-19 12:48:36 +0530

    extend functionality

--- efficient-zipper-164/src/Components/EmptyCartPage.jsx
+++ efficient-zipper-164/src/Components/EmptyCartPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Grid, GridItem, Img,Text} from '@chakra-ui/react'
+import { Grid, GridItem, Img,Text,Flex} from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+import { ArrowRightIcon } from '@chakra-ui/icons'
 function EmptyCartPage() {
   return (
     <div>
@@ -8,7 +10,7 @@ function EmptyCartPage() {
     <div style={{width:"250px",height:"300px" ,margin:"auto"}}>
     <Img src='https://constant.myntassets.com/checkout/assets/img/empty-bag.webp' width={"250px"} height={"250px"} margin={"auto"} alignItems={"center"} />
 <Text width={"200px"}  style={{display:"flex",alignItems:"center",marginLeft:"100px"} }as="b">Hey, it feels so light!</Text>
-
+<Link to="/men"><Flex width={"300px"} justifyContent={"center"} alignItems={"center"}><Text width={"300px"}  style={{display:"flex",alignItems:"center",marginLeft:"100px"} }as="b">Continue Shopping<ArrowRightIcon/></Text></Flex></Link>
     </div>
 
   </GridItem>
